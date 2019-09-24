6fbe2f8724e9227170c2b0cd2d295108d9e6496c
Yuna <yn8206@gmail.com>
2019-09-24 11:41:11 +0900

    testtest

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  publicPath : '//yn41.github.io/todolist/dist/',
+  // publicPath : '//yn41.github.io/todolist/dist/',
+  publicPath: process.env.NODE_ENV === 'production'?'/todolist/':'/'
+  ,
   css: {
     sourceMap: true
   },
-
   outputDir: 'dist'
 }
