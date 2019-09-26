08f593847adad088400db8f98ae06c4c8daa3c58
Yuna <yn8206@gmail.com>
2019-09-26 14:58:06 +0900

    달력작업

---
+++ src/components/CalenderVeiw.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="wrap">
+        <div class="tit"> 
+            <div class="bx sun"><b>일요일</b></div>
+            <div class="bx">월요일</div>
+            <div class="bx">화요일</div>
+            <div class="bx">수요일</div>
+            <div class="bx">목요일</div>
+            <div class="bx">금요일</div>
+            <div class="bx sat"><b>토요일</b></div>
+        </div>
+        <div class="cont">
+            <div class="bx" v-for="item in list" :key="item.date" v-bind:class="item.day == 0?'sun':(item.day == 6?'sat':'')">
+                <b>{{item.date}}</b>
+                <!-- <div class="cont">
+                    내용
+                </div> -->
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        'list':Array,
+        'month':Number
+    },
+    created(){
+    }
+}
+</script>
--- src/views/Calender.vue
+++ src/views/Calender.vue
@@ -1,19 +1,68 @@
 <template>
   <div class="calender">
-    <h1>달력!</h1>
+    <h1>{{month}}월</h1>
+    <CalenderVeiw :list="list"/>
   </div>
 </template>
 <script>
+import CalenderVeiw from '@/components/CalenderVeiw.vue';
+
 export default {
   data(){
     return {
+      list:[],
+      month:0,
+      day: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
+    }
+  },
+  methods:{
+    settingCalendar(){
+      let date = new Date();
+      let toDay = {
+        year : date.getFullYear(),        
+        month: date.getMonth()+1,      
+        date: date.getDate(),
+        day: date.getDay()
+      }
+      let doDate = new Date(date.getFullYear(),date.getMonth(), 1);
+      let lastDate = new Date(date.getFullYear(),date.getMonth()+1, 0);
+      this.month = doDate.getMonth()+1;
 
+      for(let i = 1; i <= lastDate.getDate(); i++){
+        let temp = new Date(date.getFullYear(),date.getMonth(),i);
+        let tempData = {
+          year : temp.getFullYear(),
+          month: temp.getMonth()+1,
+          date: temp.getDate(),
+          day: temp.getDay()
+        }
+        this.list[i-1] = tempData;
+        // if(i !== 1 && tempData.day == 6){
+        //   console.log("토욜입니다.");
+        // }
+      }
+      if(doDate.getDay() !== 0){
+        for(let i = 0; i<7; i++){
+          let tempData = {
+            year : doDate.getFullYear(),
+            month: doDate.getMonth()+1,
+            date: 0,
+            day: null
+          }
+          if(i == doDate.getDay())
+            break;
+          else
+            this.list.unshift(tempData);
+        }
+      }
+      console.log(this.list);
     }
   },
   created(){
-    let date = new Date();
-    console.log(date.getMonth());
-    console.log(date.getMonth()+1);
-  }
+    this.settingCalendar();
+  },
+  components: {
+    CalenderVeiw,
+  },
 }
 </script>
