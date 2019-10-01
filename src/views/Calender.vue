578e56e4688beace7f9ad6da8c717ec42db1f4d5
Yuna <yn8206@gmail.com>
2019-10-02 08:21:12 +0900

    test

--- src/views/Calender.vue
+++ src/views/Calender.vue
@@ -49,6 +49,7 @@ export default {
       solMonth = this.setDate(solMonth);
       //이 api는 2015년 ~ 2020년 공휴일만 가져옴
       const baseURI = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo'; 
+      // const baseURI = '//localhost:8080/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo';
       axios.get(baseURI, {
         params:{
           'serviceKey' : this.serviceKey,
